9d081c5170b0e7fc449d670a8afb0b1e1c6d8561
Georgiy <lazarev.georgiy9936@gmail.com>
2026-06-24 21:45:26 +0400

    Исправлен Enum

--- src/test/java/enumUI/TableType.java
+++ src/test/java/enumUI/TableType.java
@@ -12,7 +12,7 @@ public enum TableType {
     READ_ALL_USERS("Read all", List.of("ID", "First", "Last", "Age", "Sex", "Money")),
     READ_ALL_CARS("Read all", List.of("ID", "Engine", "Mark", "Model", "Price")),
     CREATE_NEW_CARS("Create new", List.of("ID", "Engine", "Mark", "Model", "Price")),
-    CREATE_NEW_USER("Create new", List.of("ID", "First", "Last", "Age", "Sex", "Money"));
+    CREATE_NEW_USER("Create new", List.of("ID", "First", "Last", "Age", "Sex", "Money")),
     CREATE_NEW_HOUSES("Create new", List.of("ID", "Floors", "Price"));
 
 
