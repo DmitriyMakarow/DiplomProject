98b5f964b5d5f6784b5518d77f1b8c5907cab8be
Kadirmyatova Anastasiya <kadyrmatovaanastasia12@gmail.com>
2026-07-10 13:45:57 +0300

    попытка имени в отчете

--- src/test/java/tests/ui/users/AddMoneyTest.java
+++ src/test/java/tests/ui/users/AddMoneyTest.java
@@ -11,9 +11,8 @@ import tests.ui.base.BaseTest;
 @Feature("Действия с балансом")
 public class AddMoneyTest extends BaseTest {
     @Owner("Квасникова О.Н.")
-    @Test(testName = "Добавление денег существующему пользователю",
-          groups = {"regression"})
-    @Description("Проверка корректности зачисления денежных средств на баланс существующего пользователя через интерфейс")
+    @Test(groups = {"regression"})
+    @Description("Добавление денег существующему пользователю")
         public void addMoneyPositiveTest() {
     loginPage
                 .authorization(user, password)
@@ -25,8 +24,7 @@ public class AddMoneyTest extends BaseTest {
     }
 
     @Owner("Квасникова О.Н.")
-    @Test(testName = "Снятие денег (ошибочно) у пользователя через сервис добавление денег",
-          groups = {"regression"})
+    @Test(groups = {"regression"})
     @Description("Проверка невозможности списания денежных средств через эндпоинт добавления денег")
     public void takeOffMoneyNegativeTest() {
         loginPage
@@ -39,8 +37,7 @@ public class AddMoneyTest extends BaseTest {
     }
 
     @Owner("Квасникова О.Н.")
-    @Test(testName = "Добавление денег  несуществующему пользователю",
-          groups = {"regression"})
+    @Test(groups = {"regression"})
     @Description("Проверка реакции системы при попытке добавить деньги пользователю с несуществующим ID")
     public void addMoneyToNonExistentNegativeTest() {
         loginPage
